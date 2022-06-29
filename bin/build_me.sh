83e9c0852dff62b1acae86ecb29e8392fa7d800e
Pip Grylls <pipgryllsastro@gmail.com>
2022-06-29 14:47:15 +0100

    uncommenting

--- bin/build_me.sh
+++ bin/build_me.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-#git branch -d localbuild || echo 'branch local build does not exist to delete'
-#git checkout -b localbuild
-#
-#python -m venv ./venv || echo 'venv already exists'
-#
-##TODO: Make this windows safe
-#source venv/bin/activate
-#
-#gem install github-pages bundler kramdown kramdown-parser-gfm
-#
-#python3 -m pip install --upgrade pip setuptools wheel pyyaml==5.3.1 requests
-#python3 -m pip install -r requirements.txt
-#
-##TODO: Check for rmarkdown and build it if appropriate
-#
-#python bin/make_favicons.py
-#python bin/get_schedules.py
-#python bin/get_setup.py
+git branch -d localbuild || echo 'branch local build does not exist to delete'
+git checkout -b localbuild
+
+python -m venv ./venv || echo 'venv already exists'
+
+#TODO: Make this windows safe
+source venv/bin/activate
+
+gem install github-pages bundler kramdown kramdown-parser-gfm
+
+python3 -m pip install --upgrade pip setuptools wheel pyyaml==5.3.1 requests
+python3 -m pip install -r requirements.txt
+
+#TODO: Check for rmarkdown and build it if appropriate
+
+python bin/make_favicons.py
+python bin/get_schedules.py
+python bin/get_setup.py
 
 # Build the site.
 bundle install
